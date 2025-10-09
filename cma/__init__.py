f6220385e64209335fabb532ca2abdaeeba93820
staggeredsix <108093260+staggeredsix@users.noreply.github.com>
2025-10-09 09:35:14 -0600

    Add concept memory adapter package

---
+++ cma/__init__.py
@@ -0,0 +1,7 @@
+"""Concept Memory Adapter (CMA)."""
+
+from .adapter import CMAAdapter
+from .config import CMAConfig
+from .store import ConceptMemory
+
+__all__ = ["CMAAdapter", "CMAConfig", "ConceptMemory"]
